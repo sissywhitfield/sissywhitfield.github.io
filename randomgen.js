12657f8464f680350c78ae5ff9dc09e9172e6bee
Elizabeth <sissywhitfield@gmail.com>
2018-06-12 13:40:33 -0400

    generator

--- randomgen.js
+++ randomgen.js
@@ -1,8 +1,50 @@
-	var chosenContent = "default";
-	var chosenType = "default";
+var chosenContent = "default";
+var chosenType = "default";
 
-	var totalContentAvailability = contentAvailable.length - 1;
-	var element;
+var totalContentAvailability = contentAvailable.length - 1;
+var element = "video";
+	
+var contentAvailable = [
+	"imageContent\Untitled-2_01132018.png",
+	"imageContent\Untitled-3_01132018.png",
+	"imageContent\Untitled-5_01132018.png",
+	"imageContent\Untitled_01132018.png",
+	"imageContent\lastTimeSheChecked.png",
+	"imageContent\chapter2_01132018.png",
+	"imageContent\Untitled-8_01132018.png",
+	"imageContent\Untitled-4_01132018.png",
+	"imageContent\ghostpotatogirl.png",
+	"imageContent\chapter1_01132018.png",
+	"imageContent\Untitled-1_01132018.png",
+	"imageContent\andthenshewaitedanxiously.png",
+	"imageContent\ifyousawheryouwouldknow.png",
+	"imageContentS.png",
+	"imageContent\Untitled-6_01132018.png",
+	"imageContent\BeGentle.jpg",
+	"imageContent\janet.gif",
+];
+
+var contentType = [
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+	"img",
+];
+
 	
 	function generate() {
 		var elementExists = document.getElementById("createdElement");
@@ -18,7 +60,7 @@
 		chosenType = contentType[x];
 		console.log(chosenType);
 
-		element = document.createElement([chosenType]);
+		element = document.createElement(chosenType);
 		element.setAttribute("id", "createdElement");
 		
 		if (chosenType == "img") {
